23f011fe160c4723dc442ef78dba8012ef8920de
temnovalina <59793781+temnovalina@users.noreply.github.com>
2022-05-02 15:11:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
